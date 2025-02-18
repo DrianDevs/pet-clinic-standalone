c19ef5733a05d7220374679089f3516373886454
DrianDevs <adriangm8110@gmail.com>
2025-02-18 21:20:44 +0100

    PetAdd Nombre del Owner Agregado

--- src/app/components/pet-add/pet-add.component.ts
+++ src/app/components/pet-add/pet-add.component.ts
@@ -5,6 +5,7 @@ import { PetService } from '../../services/pet.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PetType } from '../../models/pettype';
 import { Owner } from '../../models/owner';
+import { OwnerService } from '../../services/owner.service';
 
 @Component({
   selector: 'app-pet-add',
@@ -16,9 +17,11 @@ export class PetAddComponent {
   public textoBoton: string = 'Añadir';
   public pet: Pet = <Pet>{};
   public tipos: PetType[] = [];
+  public ownerName: string = '';
 
   constructor(
     private peticion: PetService,
+    private peticionOwner: OwnerService,
     private ruta: Router,
     private route: ActivatedRoute
   ) {}
@@ -37,6 +40,8 @@ export class PetAddComponent {
 
     if (this.route.snapshot.params['id'] == -1) {
       this.textoBoton = 'Añadir';
+
+      this.obtenerOwner();
     } else {
       this.textoBoton = 'Modificar';
 
@@ -86,10 +91,27 @@ export class PetAddComponent {
         this.pet.birthDate = data.birthDate;
         this.pet.type = data.type;
         this.pet.owner = data.owner;
+
+        this.ownerName = data.owner.firstName + ' ' + data.owner.lastName;
         console.log('this.pet', this.pet);
       },
       error: (error) => {
         console.error('Error al obtener la mascota', error);
+      },
+    });
+  }
+
+  obtenerOwner() {
+    this.peticionOwner.selOwnerId(this.pet.owner.id).subscribe({
+      next: (data) => {
+        console.log('data', data);
+        this.pet.owner = data;
+        this.ownerName = data.firstName + ' ' + data.lastName;
+        console.log('this.ownerName', this.ownerName);
+        console.log('this.pet', this.pet);
+      },
+      error: (error) => {
+        console.error('Error al obtener el owner', error);
       },
     });
   }
